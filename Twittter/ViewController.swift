3e4fe5c8109c7cd4ac006f6c3e4518ecf771dc4b
baifumei <k_katerina_k@icloud.com>
2024-05-29 15:44:26 -0700

    Redesign sign in screen

--- Twittter/ViewController.swift
+++ Twittter/ViewController.swift
@@ -8,44 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let userImage = UIImageView()
+    let logo = UIImageView()
+    
+    let welcomeText = UILabel()
     
     let signIn = UILabel()
-    let login = UILabel()
     let loginTextField = UITextField()
-    let password =  UILabel()
     let passwordTextField = UITextField()
+    let toggleVisibilityButton = UIButton(type: .custom)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Настройка userImage
-        setUserImage()
-        
-        // Настройка Sign in
+        // Настройка
+        setLogo()
+        setWelcomeText()
         setSignIn()
     }
 
-    func setUserImage() {
-        userImage.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(userImage)
+    func setLogo() {
+        logo.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(logo)
         
-        // Установка изображения
-        userImage.image = UIImage(named: "defaultImage")
-        userImage.contentMode = .scaleAspectFill
+        // Установка logo
+        logo.image = UIImage(named: "blueLogo")
+        logo.contentMode = .scaleAspectFill
         
-        userImage.widthAnchor.constraint(equalToConstant: 120).isActive = true
-        userImage.heightAnchor.constraint(equalToConstant: 120).isActive = true
-        userImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 90).isActive = true
-        userImage.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        logo.widthAnchor.constraint(equalToConstant: 35).isActive = true
+        logo.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        logo.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
+        logo.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    
+    func setWelcomeText() {
+        welcomeText.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(welcomeText)
         
-        // Делаем userImage круглым
-        userImage.layer.cornerRadius = 60
-        userImage.layer.masksToBounds = true
+        welcomeText.text = """
+See what's 
+happening in the
+world right now.
+"""
+        welcomeText.numberOfLines = 3
+        welcomeText.font = .systemFont(ofSize: 30, weight: .heavy)
+        welcomeText.textAlignment = .left
         
-        // Добавляем рамку
-        userImage.layer.borderWidth = 2
-        userImage.layer.borderColor = UIColor.systemIndigo.cgColor
+        welcomeText.topAnchor.constraint(equalTo: logo.bottomAnchor, constant: 60).isActive = true
+        welcomeText.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
+        welcomeText.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
     }
     
     func setSignIn() {
@@ -54,47 +64,48 @@ class ViewController: UIViewController {
         view.addSubview(signIn)
         
         signIn.text = "Sign In"
-        signIn.font = .systemFont(ofSize: 30, weight: .bold)
-        signIn.topAnchor.constraint(equalTo: userImage.bottomAnchor, constant: 20).isActive = true
-        signIn.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15).isActive = true
+        signIn.font = .systemFont(ofSize: 28, weight: .bold)
+        signIn.textAlignment = .center
+        
+        signIn.topAnchor.constraint(equalTo: welcomeText.bottomAnchor, constant: 80).isActive = true
+        signIn.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
+        signIn.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
         
         //Login
-        login.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(login)
-        
-        login.text = "Login"
-        login.font = .systemFont(ofSize: 18, weight: .regular)
-        login.topAnchor.constraint(equalTo: signIn.bottomAnchor, constant: 20).isActive = true
-        login.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15).isActive = true
-        
-        //Login TextField
         loginTextField.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(loginTextField)
         
         loginTextField.placeholder = "Enter your login or email"
         loginTextField.borderStyle = .roundedRect
-        loginTextField.topAnchor.constraint(equalTo: login.bottomAnchor, constant: 15).isActive = true
-        loginTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15).isActive = true
-        loginTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15).isActive = true
+        
+        loginTextField.topAnchor.constraint(equalTo: signIn.bottomAnchor, constant: 15).isActive = true
+        loginTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
+        loginTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
         
         //Password
-        password.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(password)
-        
-        password.text = "Password"
-        password.font = .systemFont(ofSize: 18, weight: .regular)
-        password.topAnchor.constraint(equalTo: loginTextField.bottomAnchor, constant: 20).isActive = true
-        password.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15).isActive = true
-        
-        //Password TextField
         passwordTextField.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(passwordTextField)
         
         passwordTextField.placeholder = "Enter your password"
+        passwordTextField.isSecureTextEntry = true
         passwordTextField.borderStyle = .roundedRect
-        passwordTextField.topAnchor.constraint(equalTo: password.bottomAnchor, constant: 15).isActive = true
-        passwordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15).isActive = true
-        passwordTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15).isActive = true
+        
+        passwordTextField.topAnchor.constraint(equalTo: loginTextField.bottomAnchor, constant: 15).isActive = true
+        passwordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
+        passwordTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
+        
+        
+    }
+    
+    func createConfirmButton() {
+        let signInButton = UIButton()
+        signInButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(signInButton)
+        
+        signInButton.titleLabel?.text = "Sign in"
+        signInButton.titleLabel?.font = .systemFont(ofSize: 18, weight: .regular)
+        signInButton.backgroundColor = UIColor.systemIndigo
+        
     }
 }
 
