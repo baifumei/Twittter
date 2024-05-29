761adeb586093057e0d7a429484f9c8a8f8fa14f
baifumei <k_katerina_k@icloud.com>
2024-05-29 01:49:14 -0700

    Create a launch screen

---
+++ Twittter/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Twittter
+//
+//  Created by Екатерина К on 5/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
