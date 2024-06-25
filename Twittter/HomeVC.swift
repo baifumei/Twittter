c2e102a571dd2874d876e023bde8b51eae6c48b0
baifumei <k_katerina_k@icloud.com>
2024-06-24 21:09:24 -0700

    Add tableView with tweets to the home screen

--- Twittter/HomeVC.swift
+++ Twittter/HomeVC.swift
@@ -7,11 +7,61 @@
 
 import UIKit
 
-class HomeVC: UIViewController {
+class HomeVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let logo = UIImageView()
+    var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         
-        view.backgroundColor = .blue
+        setLogo()
+        createTwitBoard()
+    }
+    
+    func setLogo() {
+        logo.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(logo)
+        
+        // Установка logo
+        logo.image = UIImage(named: "blueLogo")
+        logo.contentMode = .scaleAspectFill
+        
+        logo.widthAnchor.constraint(equalToConstant: 35).isActive = true
+        logo.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        logo.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 5).isActive = true
+        logo.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    
+    func upperBar() {
+        
+    }
+   
+    func createTwitBoard() {
+        tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: logo.bottomAnchor, constant: 10),
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Tweet: \(indexPath.row)"
+        return cell
     }
 }
