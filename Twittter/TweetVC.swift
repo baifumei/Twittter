c0d20aa0615169a8aea2f018d4a220dc21fba201
baifumei <k_katerina_k@icloud.com>
2024-07-04 01:25:00 -0700

    Add a tweet text view

--- Twittter/TweetVC.swift
+++ Twittter/TweetVC.swift
@@ -8,13 +8,24 @@
 import UIKit
 
 
-class TweetVC: UIViewController {
+class TweetVC: UIViewController, UITextViewDelegate {
+    
+    let tweetTextView: UITextView = {
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.font = UIFont.systemFont(ofSize: 16)
+        textView.textColor = UIColor.lightGray
+        textView.text = "What's happening?"
+        return textView
+    }()
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         
         setupCloseButton()
         setupTweetButton()
+        setupTweetTextView()
     }
     
     func setupCloseButton() {
@@ -42,5 +53,34 @@ class TweetVC: UIViewController {
     
     @objc func postTweet() {
         //post it!
+    }
+    
+    // Text area
+    func setupTweetTextView() {
+        view.addSubview(tweetTextView)
+        
+        NSLayoutConstraint.activate([
+            tweetTextView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            tweetTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            tweetTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            tweetTextView.heightAnchor.constraint(equalToConstant: 200)
+        ])
+        
+        tweetTextView.delegate = self
+    }
+    
+    // UITextViewDelegate methods
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == UIColor.lightGray {
+            textView.text = nil
+            textView.textColor = UIColor.black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = "What's happening?"
+            textView.textColor = UIColor.lightGray
+        }
     }
 }
