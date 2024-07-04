54a585908215727122455448d5f4a98a03f2a7df
baifumei <k_katerina_k@icloud.com>
2024-07-04 00:53:17 -0700

    Add a tweet button to post a tweet

--- Twittter/TweetVC.swift
+++ Twittter/TweetVC.swift
@@ -14,6 +14,7 @@ class TweetVC: UIViewController {
         view.backgroundColor = .white
         
         setupCloseButton()
+        setupTweetButton()
     }
     
     func setupCloseButton() {
@@ -24,5 +25,22 @@ class TweetVC: UIViewController {
     @objc func closeTapped() {
         // Закрытие модального представления
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    func setupTweetButton() {
+        let button = UIButton()
+        button.setTitle("Tweet", for: .normal)
+        button.backgroundColor = UIColor(named: "mainColor")
+        button.tintColor = .white
+        button.frame = CGRect(x: 0, y: 0, width: 70, height: 20)
+        button.addTarget(self, action: #selector(postTweet), for: .touchUpInside)
+        button.layer.cornerRadius = 16
+        
+        let tweetButton = UIBarButtonItem(customView: button)
+        self.navigationItem.rightBarButtonItem = tweetButton
+    }
+    
+    @objc func postTweet() {
+        //post it!
     }
 }
